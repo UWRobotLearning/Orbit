d1afd8919f9fe4ea130792baed41543739cfb14d
romesco <rosario@cs.uw.edu>
2023-04-22 18:18:48 -0700

    similarly to orbit, set version the conventional way

--- source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/__init__.py
+++ source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/__init__.py
@@ -32,18 +32,8 @@ import gym
 import os
 import toml
 
-# Conveniences to other module directories via relative paths
-ORBIT_ENVS_EXT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
-"""Path to the extension source directory."""
-
-ORBIT_ENVS_DATA_DIR = os.path.join(ORBIT_ENVS_EXT_DIR, "data")
-"""Path to the extension data directory."""
-
-ORBIT_ENVS_METADATA = toml.load(os.path.join(ORBIT_ENVS_EXT_DIR, "config", "extension.toml"))
-"""Extension metadata dictionary parsed from the extension.toml file."""
-
 # Configure the module-level variables
-__version__ = ORBIT_ENVS_METADATA["package"]["version"]
+__version__ = "0.2.3" 
 
 ##
 # Classic control
